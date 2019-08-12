8e6a61ae6787dce6c82a99b2625eb9117f401499
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-08-12 11:55:22 -0700

    Agregar archivos de proyecto.

---
+++ EjercicioLibreria/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Emgu;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace EjercicioLibreria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
